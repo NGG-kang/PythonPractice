5056ba7d8e6c44bba3bcc8113cd01139af1726f1
NGG-Kang <skarndrkd1222@gmail.com>
2021-01-24 11:30:46 +0900

    1/24 Python Practice

---
+++ Variable.py
@@ -0,0 +1,16 @@
+a = 1 ,-1, 0        # 정수 (Integer)
+a = 1.123, -1.34    # 실수 (Floating-point)
+a = 0o34, 0o12      # 8진수 (Octal) 0o로 시작
+a = 0x2A, 0xFF      # 16진수 (Hexadecimal) 0x로 시작
+
+
+#사칙연산
+a+a                 # 덧셈
+a-a                 # 뺄셈
+a*a                 # 곱셈
+a/a                 # 나눗셈, 몫 반환
+a%a                 # 나머지 연산
+a//a                # 몫의 정수값 반환
+a**a                # 제곱 연산
+
+
